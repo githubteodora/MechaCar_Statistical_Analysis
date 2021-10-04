5363869b2e7361bf8be0bda3a077b5445bbd4cbb
githubteodora <83152554+githubteodora@users.noreply.github.com>
2021-10-03 19:35:57 -0700

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,12 +1,13 @@
 # rename MechaCarChallenge.RScript
 # mpg = miles per gallon
 # AWD = All wheel drive
+# PSI = pound per square inch
 
-library(GGally)
+library(GGally) #a very cool library for visualizing correlation and distribution
 library(dplyr)
 
 cars_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-ggpairs(cars_table) # a cool library that visualizes correlation coeff.
+ggpairs(cars_table) # a cool tool that visualizes correlation coeff.
 
 multiple_reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, cars_table)
 summary(multiple_reg)
@@ -18,7 +19,7 @@ summary(multiple_reg)
 # When assessing how well the model fit the data, you should look for a symmetrical distribution across these points on the mean value zero (0)
 
 # Coefficient-Estimate: This is a one unit increase in X then expected change in Y;
-# in our case, y = mpg , x are the rest opf the columns
+# in our case, y = mpg , x are the rest of the columns
 
 # Coefficient-Std. Error: The standard deviation of an estimate is called the standard error. The standard error of the coefficient measures how precisely the model estimates the coefficient's unknown value. The standard error of the coefficient is always positive.
 # We'd ideally want a lower number relative to its coefficients. Also used for checking confidence interval;
@@ -35,7 +36,7 @@ summary(multiple_reg)
 # Asterisks mark aside p value define significance of value;
 # Note the 'signif. Codes' associated to each estimate. Three stars (or asterisks) represent a highly significant p-value. Consequently, a small p-value for the intercept and the slope indicates that we can reject the null hypothesis which allows us to conclude that there is a relationship between speed and distance.
 
-
-
-
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension_table %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), var_PSI=var(PSI), STDEV_PSI=sd(PSI))
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), variance_PSI=var(PSI), STDEV_PSI=sd(PSI), .groups = 'keep')
 
